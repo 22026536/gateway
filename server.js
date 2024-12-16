701e6a4ac0b6f280838bf434756f1ff67f1c21ac
22026536 <22026536>
2024-12-16 16:47:43 +0700

    'quang'

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(json());
 app.set('trust proxy', 1);
 app.use(cookieParser());
 
-app.use("/user", proxy("https://animeTangoUserService.onrender.com"));
-app.use("/anime", proxy("https://animeTangoAnimeService.onrender.com"));
-app.use("/useranime", proxy("https://animeTangoUserAnimeService.onrender.com"));
-app.use("/admin", proxy("https://animeTangoAdminService.onrender.com"));
-app.use("/knn", proxy("https://animeTangoKNNService.onrender.com"));
-app.use("/decisiontree", proxy("https://animeTangoDecisionTreeService.onrender.com"));
-app.use("/naivebayes", proxy("https://animeTangoNaiveBayesService.onrender.com"));
+app.use("/user", proxy("https://animetangouserservice.onrender.com"));
+app.use("/anime", proxy("https://animetangoanimeservice.onrender.com"));
+app.use("/useranime", proxy("https://animetangouseranimeservice.onrender.com"));
+app.use("/admin", proxy("https://animetangoadminservice.onrender.com"));
+app.use("/knn", proxy("https://animetangoknnservice.onrender.com"));
+app.use("/decisiontree", proxy("https://animetangodecisiontreeservice.onrender.com"));
+app.use("/naivebayes", proxy("https://animetangonaivebayesservice.onrender.com"));
 
 
 const PORT = 3004;
