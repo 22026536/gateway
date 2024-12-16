307d0b85ba88cdebb21eba348def43b26aad4b5d
22026536 <22026536>
2024-12-16 17:10:04 +0700

    'quang'

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/user", proxy("https://animetangouserservice.onrender.com"));
-app.use("/anime", proxy("https://animetangoanimeservice.onrender.com"));
-app.use("/useranime", proxy("https://animetangouseranimeservice.onrender.com"));
-app.use("/admin", proxy("https://animetangoadminservice.onrender.com"));
+app.use("/user", proxy("https://animetangouserservice.onrender.com/user"));
+app.use("/anime", proxy("https://animetangoanimeservice.onrender.com/anime"));
+app.use("/useranime", proxy("https://animetangouseranimeservice.onrender.com/useranime"));
+app.use("/admin", proxy("https://animetangoadminservice.onrender.com/admin"));
 app.use("/knn", proxy("https://animetangoknnservice.onrender.com"));
 app.use("/decisiontree", proxy("https://animetangodecisiontreeservice.onrender.com"));
 app.use("/naivebayes", proxy("https://animetangonaivebayesservice.onrender.com"));
